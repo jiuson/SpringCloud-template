83a6ae57abab8097d514c74573bb4216ec63cf77
zuowenjie <zuowenjie@sunlands.com>
2018-11-14 17:07:52 +0800

    搭建Eureka客户端

---
+++ springcloud2.0-yipinketang-parent/springcloud2.0-yipinketang-eureka-client/src/main/java/com/yipinketang/eurekaclient/controller/EurekaClientController.java
@@ -0,0 +1,13 @@
+package com.yipinketang.eurekaclient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EurekaClientController {
+
+    @GetMapping("/getServiceMethod")
+    public String getServiceMethod(){
+        return "this is eureka client";
+    }
+}
