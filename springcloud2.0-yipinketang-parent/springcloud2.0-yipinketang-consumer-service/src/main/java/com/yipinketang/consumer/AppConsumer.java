360ce19c6a58f73757c7c9157f53354067dd6ca0
zuowenjie <zuowenjie@sunlands.com>
2018-11-15 16:17:29 +0800

    SpringBoot web组件的RestTemplate访问微服务接口

--- springcloud2.0-yipinketang-parent/springcloud2.0-yipinketang-consumer-service/src/main/java/com/yipinketang/consumer/AppConsumer.java
+++ springcloud2.0-yipinketang-parent/springcloud2.0-yipinketang-consumer-service/src/main/java/com/yipinketang/consumer/AppConsumer.java
@@ -2,7 +2,11 @@ package com.yipinketang.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,5 +14,11 @@ public class AppConsumer {
 
     public static void main(String[] args){
         SpringApplication.run(AppConsumer.class, args);
+    }
+
+    @Bean//通过注解的方式将RestTemplate对象注入到Spring的IOC容器中
+    @LoadBalanced
+    public RestTemplate generateRestTemplate(){
+        return new RestTemplateBuilder().build();
     }
 }
