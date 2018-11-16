6c7923fb7c644ddf0f1ed09021414db7c4e3e374
zuowenjie <zuowenjie@sunlands.com>
2018-11-16 11:28:56 +0800

    添加@EnableFeignClients ribbon、feign配置

--- springcloud2.0-yipinketang-parent/springcloud2.0-yipinketang-consumer-service/src/main/java/com/yipinketang/consumer/AppConsumer.java
+++ springcloud2.0-yipinketang-parent/springcloud2.0-yipinketang-consumer-service/src/main/java/com/yipinketang/consumer/AppConsumer.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient//开启Eureka客户端功能
+@EnableFeignClients//开启feign客户端功能
 public class AppConsumer {
 
     public static void main(String[] args){
@@ -17,7 +19,7 @@ public class AppConsumer {
     }
 
     @Bean//通过注解的方式将RestTemplate对象注入到Spring的IOC容器中
-    @LoadBalanced
+    @LoadBalanced//开启RestTemplate实例的负载均衡器
     public RestTemplate generateRestTemplate(){
         return new RestTemplateBuilder().build();
     }
