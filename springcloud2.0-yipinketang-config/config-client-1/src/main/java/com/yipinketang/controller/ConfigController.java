3c855159f8b7153a5039d1e2014209d9d30aaae9
zuowenjie <zuowenjie@sunlands.com>
2018-12-03 12:28:59 +0800

    SpringCloud config客户端

--- springcloud2.0-yipinketang-config/config-client-1/src/main/java/com/yipinketang/controller/ConfigController.java
+++ springcloud2.0-yipinketang-config/config-client-1/src/main/java/com/yipinketang/controller/ConfigController.java
@@ -2,10 +2,12 @@ package com.yipinketang.controller;
 
 import com.yipinketang.generalResponse.GeneralResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigController {
 
     @Value("${hello}")
