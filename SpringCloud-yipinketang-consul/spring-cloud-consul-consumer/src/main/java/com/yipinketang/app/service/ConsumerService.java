46a94456d6c69bcc2bbcf4bce800b187f353674d
zuowenjie <zuowenjie@sunlands.com>
2018-12-11 19:10:38 +0800

    get all service from consul

--- SpringCloud-yipinketang-consul/spring-cloud-consul-consumer/src/main/java/com/yipinketang/app/service/ConsumerService.java
+++ SpringCloud-yipinketang-consul/spring-cloud-consul-consumer/src/main/java/com/yipinketang/app/service/ConsumerService.java
@@ -1,11 +1,15 @@
 package com.yipinketang.app.service;
 
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/consul-consumer")
@@ -13,9 +17,14 @@ public class ConsumerService {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/hello")
     public Object getServiceFromProducer(@RequestParam(name = "name", required = false) String name){
+
+        List<String> services = discoveryClient.getServices();
+        System.out.println(services);
         String url = "http://producer/consul-producer/hello";
         return restTemplate.getForObject(url, String.class, name);
     }
