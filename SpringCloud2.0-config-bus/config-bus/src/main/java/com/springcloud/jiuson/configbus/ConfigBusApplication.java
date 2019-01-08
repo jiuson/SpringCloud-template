032cb1559af154ca64b81e33fa22ddfebbf5fd0b
zuowenjie <zuowenjie@sunlands.com>
2019-01-08 17:22:39 +0800

    bus

---
+++ SpringCloud2.0-config-bus/config-bus/src/main/java/com/springcloud/jiuson/configbus/ConfigBusApplication.java
@@ -0,0 +1,14 @@
+package com.springcloud.jiuson.configbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigBusApplication.class, args);
+    }
+
+}
+
---
+++ SpringCloud2.0-config-bus/config-bus/src/test/java/com/springcloud/jiuson/configbus/ConfigBusApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springcloud.jiuson.configbus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigBusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
