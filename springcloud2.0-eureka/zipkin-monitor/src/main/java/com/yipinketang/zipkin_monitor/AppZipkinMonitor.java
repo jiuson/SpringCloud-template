b83e1290d6e1a5f2432d57601a466187a86ce63e
zwj <598507757@qq.com>
2019-11-17 10:24:54 +0800

    zuul gateway

---
+++ springcloud2.0-eureka/zipkin-monitor/src/main/java/com/yipinketang/zipkin_monitor/AppZipkinMonitor.java
@@ -0,0 +1,21 @@
+package com.yipinketang.zipkin_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+/**
+ * description: AppZipkinMonitor <br>
+ * date: 2019/11/13 16:08 <br>
+ * author: user <br>
+ * version: 1.0 <br>
+ */
+
+@EnableZipkinServer
+@SpringBootApplication
+public class AppZipkinMonitor {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppZipkinMonitor.class, args);
+    }
+}
