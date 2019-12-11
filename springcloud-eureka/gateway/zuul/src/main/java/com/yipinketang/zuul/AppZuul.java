94677fe8c189eb2d574a0b9057bd24ae53b89218
zwj <598507757@qq.com>
2019-12-11 11:25:37 +0800

    重构zuul-gateway

--- springcloud-eureka/gateway/zuul/src/main/java/com/yipinketang/zuul/AppZuul.java
+++ springcloud-eureka/gateway/zuul/src/main/java/com/yipinketang/zuul/AppZuul.java
@@ -1,12 +1,13 @@
-package com.yipinketang.zuul_gateway;
+package com.yipinketang.zuul;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
- * description: AppZuulGateway <br>
- * date: 2019/11/13 9:25 <br>
+ * description: AppZuul <br>
+ * date: 2019/12/11 11:20 <br>
  * author: jiuson <br>
  * version: 1.0 <br>
  */
@@ -14,5 +15,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
-public class AppZuulGateway {
+public class AppZuul {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppZuul.class,args);
+    }
 }
