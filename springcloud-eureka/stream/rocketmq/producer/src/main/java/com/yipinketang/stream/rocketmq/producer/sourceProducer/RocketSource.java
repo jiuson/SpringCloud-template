7517c4c74a5728db808868e2ad1e71aca1042d7d
zwj <598507757@qq.com>
2019-12-12 09:25:23 +0800

    更改项目名，添加注释

---
+++ springcloud-eureka/stream/rocketmq/producer/src/main/java/com/yipinketang/stream/rocketmq/producer/sourceProducer/RocketSource.java
@@ -0,0 +1,20 @@
+package com.yipinketang.stream.rocketmq.producer.sourceProducer;
+
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+/**
+ * description: RocketSource <br>
+ * date: 2019/11/28 14:21 <br>
+ * author: jiuson <br>
+ * version: 1.0 <br>
+ */
+public interface RocketSource {
+
+    String OUTPUT = "output1";
+
+    @Output(OUTPUT)
+    MessageChannel output();
+
+
+}
