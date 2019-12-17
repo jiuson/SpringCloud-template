30b08c7dd2a6190cb1f466d4f5c49de54a61b8f8
zwj <598507757@qq.com>
2019-12-17 16:19:00 +0800

    rebuild

---
+++ springcloud-nacos/redis/src/main/java/com/yipinketang/redis/AppRedis.java
@@ -0,0 +1,19 @@
+package com.yipinketang.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description: AppRedis <br>
+ * date: 2019/12/16 14:57 <br>
+ * author: jiuson <br>
+ * version: 1.0 <br>
+ */
+
+@SpringBootApplication
+public class AppRedis {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRedis.class, args);
+    }
+}
